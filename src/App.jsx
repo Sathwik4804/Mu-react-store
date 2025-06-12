307e6a352c92ba9580c7da45500355a0ff5c841c
Dachepally Sathwik <sathwikdachepally@gmail.com>
2025-06-12 14:02:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Logout from "./Logout";
 import Order from "./order";
 import Register from "./Register";
 import Test from "./Test";
-import Product from "./product";
+import Product from "./Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 export const AppContext = createContext();
