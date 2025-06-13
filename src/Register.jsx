a2478ed0d44a91fef65b9dc34673367e3f845f3c
Sathwik4804 <sathwikdachepally@gmail.com>
2025-06-13 11:08:05 +0530

    updated

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,46 +1,60 @@
-import React from "react";
-import { Link,useNavigate } from "react-router-dom";
-import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext, useRef } from "react";
 import { AppContext } from "./App";
+// import "./Register.css";
+import axios from "axios";
+
 export default function Register() {
   const [user, setUser] = useState({});
-  const Navigate = useNavigate()
-  const { users, setUsers } = useContext(AppContext);
-  const handleSubmit = () => {
-    setUsers([...users, user]);
-    Navigate("/login")
-  };
+  const{users, setUsers} = useContext(AppContext);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const navigate = useNavigate(); 
+
+const handleRegister = async () => {
+  const url = "http://localhost:8080/register";
+  try {
+    const res = await axios.post(url, user);
+    setUsers([...users, res.data]); 
+    navigate("/login");
+  } catch (error) {
+    console.error("Registration failed:", error);
+  }
+};
+
+
   return (
-    <div>
-      <h2>Register</h2>
-      <p>
-        <input
-          type="text"
-          placeholder="Enter Name"
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
-        />
-      </p>
-      <p>
-        <input
-          type="text"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="Enter Email Address"
-        />
-      </p>
-      <p>
-        <input
-          type="password"
-          onChange={(e) => setUser({ ...user, pass: e.target.value })}
-          placeholder="New Password"
-        />
-      </p>
-      <p>
-        <button onClick={handleSubmit}>Submit</button>
-      </p>
-      <hr />
-      <p>
-        <Link to="/login">Aready a member? Login Here...</Link>
-      </p>
-    </div>
+    <div className="register-container">
+      <div className="register-box">
+        <h2>Register</h2>
+        <p>
+          Name:
+          <input
+            type="text"
+            placeholder="Enter Name"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          />
+        </p>
+        <p>
+          Email Id:
+          <input
+            type="text"
+            placeholder="Enter Email Id"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </p>
+        <p>
+          Password:
+          <input
+            type="password"
+            placeholder="New Password"
+            onChange={(e) => setUser({ ...user, pass: e.target.value })}
+          />
+        </p>
+        <button onClick={handleRegister}>Register</button>
+        <Link to="/login">Already a member? Login here..</Link>
+      </div>
+  </div>
   );
 }
